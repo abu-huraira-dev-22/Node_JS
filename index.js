2987719463dca74b1c1e46ac8e3f066fee6436ed
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2026-04-05 16:42:44 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// const http = require('http')
+// const server = http.createServer((req,res)=>{
+//     console.log(req,'==>req')
+//     res.end('Server is open')
+// })
+// server.listen(5000,'127.0.0.1',()=>{
+//     console.log('==>Server is running')
+// })
+const fs = require('fs')
+fs.writeFileSync('dummy.txt','trying with modules')
+const os = require('os')
+console.log(os.platform())
+console.log(os.hostname())
+console.log(os.cpus())
